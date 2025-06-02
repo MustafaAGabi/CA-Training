6aa46fca4c169c037737def8bb7679fa07a4ab83
mustafagabi <mustafakabi712@gmail.com>
2025-06-02 13:13:51 +0300

    start use sql server extintion

---
+++ DoList/SQL SERVER Training/train.sql
@@ -0,0 +1,3 @@
+USE [VehicleMakesDB]
+GO
+
--- DoList/Task.cs
+++ DoList/Task.cs
@@ -3,7 +3,7 @@
 public class Task
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     
-    
+
 }
