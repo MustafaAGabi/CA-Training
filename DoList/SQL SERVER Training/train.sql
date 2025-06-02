9025d76f84b03454d7499a2c2936f98e33d9125d
mustafagabi <mustafakabi712@gmail.com>
2025-06-02 13:29:21 +0300

    Course 17 - Problem 26

--- DoList/SQL SERVER Training/train.sql
+++ DoList/SQL SERVER Training/train.sql
@@ -1,3 +1,10 @@
 USE [VehicleMakesDB]
 GO
 
+-- Problem 26
+-- Get all vehicles that their body is 'Sport Utility' and Year > 2020
+SELECT B.BodyName, VD.Vehicle_Display_Name, VD.Engine 
+FROM VehicleDetails VD
+INNER JOIN Bodies B ON VD.BodyID = B.BodyID
+WHERE B.BodyName = N'Sport Utility' AND VD.[Year] > 200
+
