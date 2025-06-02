059cfead9f275d00924ac4c0c0a45c7032aec8c6
mustafagabi <mustafakabi712@gmail.com>
2025-06-02 13:54:34 +0300

    Course 17 - Problem 28

--- DoList/SQL SERVER Training/train.sql
+++ DoList/SQL SERVER Training/train.sql
@@ -16,3 +16,12 @@ FROM VehicleDetails VD
 INNER JOIN Bodies B ON VD.BodyID = B.BodyID
 WHERE B.BodyName IN ('Coupe', 'Hatchback', 'Sedan')
 
+-- Problem 28
+-- Get all vehicales that thier Body is 'Coupe' or 'Hatchback' or 'Sedan' And manufactured in year 2008 or 2020 or 2021
+SELECT VD.ID, VD.Vehicle_Display_Name, VD.[Year], B.BodyName
+FROM VehicleDetails VD
+INNER JOIN Bodies B ON VD.BodyID = B.BodyID
+WHERE VD.[Year] IN  (2008, 2020, 2021) AND B.BodyName IN ('Coupe', 'Hatchback', 'Sedan') 
+ORDER BY VD.Vehicle_Display_Name
+
+
